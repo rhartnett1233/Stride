e829be86c715fc433bee2b0c28dd42d0fdd62e00
rhartnett1233 <rhartnett@umass.edu>
2017-11-09 11:33:39 -0500

    App can read in users and is connected to noSQL database

---
+++ Application Code/SDP_Stride/app/src/main/java/com/example/richie/sdp_stride/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.richie.sdp_stride;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+
+public class MainActivity extends AppCompatActivity {
+    DynamoDBMapper dynamoDBMapper;
+
+    String userId = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Context appContext = getApplicationContext();
+
+        AccelerometerDataDO accelerometerDataDO = new AccelerometerDataDO( appContext );
+        GyroscopeDataDO gyroscopeDataDO = new GyroscopeDataDO( appContext );
+        MagnometerDataDO magnometerDataDO = new MagnometerDataDO( appContext );
+
+    }
+
+}
